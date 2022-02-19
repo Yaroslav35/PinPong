75cf7bcbc6c432d1fd52aeb083a1c09ebc5bdac9
Yaroslav35 <99538106+Yaroslav35@users.noreply.github.com>
2022-02-19 17:11:13 +0400

    Update pinpong.py

--- pinpong.py
+++ pinpong.py
@@ -1,5 +1,12 @@
 from pygame import *
+font.init()
 finish = False
+speed_x = 2
+speed_y = 2
+win_height = 500
+font1 = font.Font(None, 35)
+win1 = font1.render('Игрок 1 выйграл!', True, (180,0,0))
+win2 = font1.render('Игрок 2 выйграл!', True, (180,0,0))
 class GameSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y, player_speed):
         super().__init__()
@@ -63,6 +70,23 @@ while game:
         platform2.reset()
         ball.reset()
         ball.update()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+            speed_y *= -1
+
+        if sprite.collide_rect(platform1, ball) or sprite.collide_rect(platform2, ball):
+            speed_x *= -1
+
+        if ball.rect.x > 695:
+            #выйграла 1 ракетка
+            window.blit(win1,(200,200))
+            finish = True
+        
+        if ball.rect.x < 1:
+            #выйграла 2 ракетка
+            window.blit(win2,(200,200))
+            finish = True
 
     clock.tick(240)
 
